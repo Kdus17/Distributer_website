4b7952d109bd8cdb534e9eaae2d443e8f333d320
Abel Gulbet <gulbetabel@gmail.com>
2025-06-20 09:41:41 +0300

    a bit more

--- src/components/Whoarewe.jsx
+++ src/components/Whoarewe.jsx
@@ -2,7 +2,7 @@
 
 const Whoarewe = () => {
   return (
-    <div>Whoarewe</div>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium exercitationem consequatur, enim dolor inventore reprehenderit fugiat quam aliquid voluptatibus? Dolorum autem iste molestias aspernatur cumque quasi asperiores unde, ut ipsam.</div>
   )
 }
 
