e6180030f9141129ff96f0938dc013a46d2c80e5
Kdus17 <kidus2460@gmail.com>
2025-06-25 11:57:57 +0300

    added validation to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,36 +4,70 @@ import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
 import { useState } from "react";
 import { FaRegUser } from "react-icons/fa6";
-import { Loader2 } from "lucide-react";
+import { Loader2, Mail,Lock } from "lucide-react";
 import ErrorCard from "../assets/ErrorCard";
+
+
 
 const Login = () => {
   const [see, setsee] = useState(false)
+  const [formData, setFromData] = useState({
+    email: "",
+    password: "",
+  })
+  const [errors, seterrors] = useState({})
+
+  const handlechange = (e) => {
+    const {name, value} = e.target;
+    setFromData((prev)=>({
+      ...prev,
+      [name]:value,
+    }))
+    if(errors[name]){
+      seterrors((prev)=>({
+        ...prev,
+        [name]: "",
+      }))
+    }
+  }
+
+  const validateform = () => {
+    const newerrors = {};
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!formData.email.trim()||!emailRegex.test(formData.email.trim())){
+      newerrors.email = "Invalid email"
+    }
+    if(!formData.password.trim()){
+      newerrors.password = "Invalid Password"
+    }
+    seterrors(newerrors)
+  }
+
   return (
     <div className="min-h-screen flex  justify-center items-center bg-gray-100">
         <div className="flex flex-col w-120 h-full pb-3 border border-gray-200 shadow-lg rounded-lg px-12 bg-white  gap-6">
           <p className="uppercase self-center my-6 font-bold text-shadow-lg text-2xl">kingdom's business</p>
 
-          {/** User name */}
+          {/** Email*/}
           <div className="flex flex-col gap-2 text-gray-400">
-            <label htmlFor="name" className="font-serif font-semibold text-black ">Username</label>
-            <label htmlFor="name" className="flex has-focus:border-red-500 text-black has-focus:text-red-500 justify-between border-2  border-gray-400 rounded-lg items-center py-2 px-2">
-              <FaRegUser />
-              <input type="text" name="username" id="name" className=" px-2 w-full caret-red-500 text-black outline-none"/>
+            <label htmlFor="email" className="font-serif font-semibold text-black ">Email</label>
+            <label htmlFor="email" className="flex has-focus:border-red-500 text-black has-focus:text-red-500 justify-between border-2  border-gray-400 rounded-lg items-center py-2 px-2">
+              <Mail/>
+              <input type="text" name="email" id="email" className=" px-2 w-full caret-red-500 text-black outline-none" onChange={handlechange}/>
             </label>
-            {/* <ErrorCard/> */}
+            {errors.email && <ErrorCard wrong={errors.email}/>}
           </div>
 
           {/**Password */}
           <div className="flex flex-col gap-2 text-gray-700">
             <label htmlFor="password" className="font-serif font-semibold text-black">Password</label>
             <label htmlFor="password" className="border-2 border-gray-400 has-focus:border-red-500 has-focus:text-red-500 rounded-lg py-2  px-2 flex justify-between  items-center">
-              <RiLockPasswordLine className="text-2xl " />
-              <input type={`${see ? "text":"password"}`} name="password" id="password" className="w-full font-sans caret-red-500 outline-none text-black px-2" />
+              <Lock />
+              <input type={`${see ? "text":"password"}`} name="password" id="password" className="w-full font-sans caret-red-500 outline-none text-black px-2" onChange={handlechange}/>
               {see && <IoMdEye onClick={()=>setsee(!see)} className="cursor-pointer text-2xl "/>}
               {!see && <IoMdEyeOff onClick={()=>setsee(!see)} className="cursor-pointer text-2xl "/>}
             </label>
-            {/* <ErrorCard/> */}
+            {errors.password && <ErrorCard wrong={errors.password}/>}
           </div>
 
           {/**Remember me */}
@@ -44,7 +78,7 @@ const Login = () => {
 
           {/**Button */}
           <div className="flex justify-center mt-6">
-            <button className="bg-red-600 w-full p-2 cursor-pointer text-white rounded-md uppercase hover:bg-red-700 flex justify-center items-center gap-2"> 
+            <button className="bg-red-600 w-full p-2 cursor-pointer text-white rounded-md uppercase hover:bg-red-700 flex justify-center items-center gap-2" onClick={validateform}> 
               {/* <Loader2 className="w-5 h-5 animate-spin" /> */}
               Sign Up</button>
           </div>
