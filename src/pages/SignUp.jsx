0b79f475daaaac28e203767f626a0ca4b02c7605
Abel Gulbet <gulbetabel@gmail.com>
2025-07-05 16:17:15 +0300

    sign up page lang

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,9 +3,12 @@ import { RiLockPasswordLine } from "react-icons/ri";
 import { useState } from "react";
 import { Loader2,Mail,Lock ,Eye,EyeClosed,User } from "lucide-react";
 import ErrorCard from "../assets/ErrorCard";
+import { useLanguageContext } from '../LanguageContext';
 import LoadingCircle from "../assets/LoadingCircle";
 
 const SignUp = () => {
+   const {Lang,toggleLang} = useLanguageContext();
+  
   const [see, setsee] = useState(false)
   const [see2, setsee2] = useState(false)
   const [errors, seterrors] = useState({})
@@ -51,19 +54,28 @@ const SignUp = () => {
     seterrors(newerrors);
     console.log(errors)
   }
-  
+  const langopt ={ 
+      t1: Lang ?  "ኪንግደም ብዝንስ": "kingdom's business",
+    t2:Lang ?"መጠቃሚያ ስም":"Username",
+    t3:Lang ?"የኢሜይል አድራሻ":"Email",
+    t4:Lang ?"የይለፍ ቃል":"Password",
+    t5:Lang ?"የይለፍ ቃልን ያረጋግጡ":"Confirm Password",
+    t6:Lang ?"መመዝገብ":"Sign Up",
+    t7:Lang ?"ቀድሞውኑ አካውንት አለዎት?":"Already have an account?",
+    t8:Lang ?"መግባት":"Login",
+  }
   return (
       <div className="min-h-screen flex  justify-center items-center bg-gray-100  dark:bg-[#121212]">
           <div className="flex flex-col w-160 h-full border border-gray-200 shadow-lg rounded-lg px-12 pb-3 bg-white  gap-6 dark:bg-gray-500">
-            <p className="uppercase self-center my-6 font-bold text-shadow-lg text-2xl select-none">kingdom's business</p>
+            <p className="uppercase self-center my-6 font-bold text-shadow-lg text-2xl select-none">{langopt.t1}</p>
 
             {/** User name */}
             <div className="flex flex-col gap-2 text-gray-700">
-              <label htmlFor="name" className="font-serif font-semibold text-black select-none">Username</label>
+              <label htmlFor="name" className="font-serif font-semibold text-black select-none">{langopt.t2}</label>
               <label htmlFor="name" className="flex has-focus:border-red-500  has-focus:text-red-500 justify-between border-2 border-gray-400 rounded-lg items-center py-2 px-2">
                 <User />
                 <input type="text" name="username" id="name" className=" px-2 w-full caret-red-500 text-black  outline-none" value={formData.username} onChange={handlechange}
-                placeholder="Enter your username"/>
+                placeholder={Lang ? "የተጠቃሚ ስምዎን ያስገቡ": "Enter your username"}/>
               </label>
               {/* <ErrorCard wrong={"Username is taken"}/> */}
               {errors.username && <ErrorCard wrong={errors.username}/>}
@@ -71,11 +83,11 @@ const SignUp = () => {
 
             {/**Email */}
             <div className="flex flex-col gap-2 text-gray-700">
-              <label htmlFor="email" className="font-serif font-semibold text-black select-none">Email</label>
+              <label htmlFor="email" className="font-serif font-semibold text-black select-none">{langopt.t3}</label>
               <label htmlFor="email" className="flex has-focus:border-red-500  has-focus:text-red-500 justify-between border-2 border-gray-400 rounded-lg items-center py-2 px-2">
                 <Mail />
                 <input type="email" name="email" id="email" className=" px-2 w-full caret-red-500 text-black outline-none" value={formData.email} onChange={handlechange}
-                placeholder="Enter your email"/>
+                placeholder={Lang ? "ኢሜይልዎን ያስገቡ": "Enter your email"}/>
               </label>
               {/* <ErrorCard wrong={"Invalid email address"}/> */}
               {errors.email && <ErrorCard wrong={errors.email}/>}
@@ -83,11 +95,11 @@ const SignUp = () => {
 
             {/**Password */}
             <div className="flex flex-col gap-2 text-gray-700">
-              <label htmlFor="password" className="font-serif font-semibold text-black select-none">Password</label>
+              <label htmlFor="password" className="font-serif font-semibold text-black select-none">{langopt.t4}</label>
               <label htmlFor="password" className="border-2 border-gray-400 has-focus:border-red-500 has-focus:text-red-500 rounded-lg py-2  px-2 flex justify-between  items-center">
                 <Lock  />
                 <input type={`${see ? "text":"password"}`} name="password" id="password" className="w-full font-sans caret-red-500 text-black outline-none  px-2" value={formData.password} onChange={handlechange}
-                placeholder="Enter your password" />
+                placeholder =  {Lang ?  "የይለፍ ቃልዎን ያስገቡ" : "Enter your password"} />
                 {see && <Eye onClick={()=>setsee(!see)} className="cursor-pointer text-2xl "/>}
                 {!see && <EyeClosed onClick={()=>setsee(!see)} className="cursor-pointer text-2xl "/>}
               </label>
@@ -96,11 +108,11 @@ const SignUp = () => {
 
             {/**Confirm_Password */}
             <div className="flex flex-col gap-2 text-gray-700">
-              <label htmlFor="confirm_password" className="font-serif font-semibold text-black select-none">Confirm Password</label>
+              <label htmlFor="confirm_password" className="font-serif font-semibold text-black select-none">{langopt.t5}</label>
               <label htmlFor="confirm_password" className="border-2 border-gray-400 has-focus:border-red-500 has-focus:text-red-500 rounded-lg py-2  px-2 flex justify-between  items-center">
                 <Lock  />
                 <input type={`${see ? "text":"password"}`} name="confirm_password" id="confirm_password" className="w-full font-sans caret-red-500 outline-none text-black  px-2" value={formData.confirm_password} onChange={handlechange}
-                placeholder="Confirm your password" />
+                placeholder={ Lang ? "የይለፍ ቃልህን አረጋግጥ" :"Confirm your password"} />
                 {see2 && <Eye onClick={()=>setsee2(!see2)} className="cursor-pointer"/>}
                 {!see2 && <EyeClosed onClick={()=>setsee2(!see2)} className="cursor-pointer"/>}
               </label>
@@ -111,13 +123,13 @@ const SignUp = () => {
             <div className="flex justify-center mt-6 select-none">
               <button className="bg-red-600 w-full p-2 cursor-pointer text-white rounded-md uppercase hover:bg-red-700 flex justify-center items-center gap-2" onClick={validateform}> 
                 {/* <Loader2 className="w-5 h-5 animate-spin" /> */}
-                Sign Up</button>
+                {langopt.t6}</button>
             </div>
 
 
             <div className="flex justify-center gap-1 items-center select-none">
-              <p className="text-gray-400">Already have an account?</p>
-              <Link to={'/login'} className="text-red-500">Login</Link>
+              <p className="text-gray-400">{langopt.t7}</p>
+              <Link to={'/login'} className="text-red-500">{langopt.t8}</Link>
             </div>
           </div>
       </div>
