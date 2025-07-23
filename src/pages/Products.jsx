f11a7ee07cb37d558626741d5f043f3c5a245887
Kdus17 <kidus2460@gmail.com>
2025-07-23 10:36:05 +0300

    ui

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -51,9 +51,9 @@ const Products = () => {
     }
   ];
   return (
-    <div className="flex flex-col items-center bg-gray-200">
+    <div className="flex flex-col items-center bg-gray-50">
    {/*   Hero  */}
-      <div className="flex flex-col gap-2 items-center bg-gradient-to-r from-red-500 to-red-700 w-full py-24 text-white mb-8">
+      <div className="flex flex-col gap-2 items-center bg-gradient-to-r from-red-500 to-red-700 w-full py-24 text-white border-b border-red-200">
         <div className='flex gap-2 pt-5'>
             <div className='bg-white/30 rounded-full'><Coffee className='w-16 h-16 p-3'/></div>
             <div className='bg-white/30 rounded-full'><Crown className='w-16 h-16 p-3 text-yellow-400'/></div>
@@ -73,21 +73,21 @@ const Products = () => {
 
       </div>
 {/*  section nav  */}
-        <div className='flex flex-col sm:flex-row justify-center gap-4 select-none'>
+        <div className='flex flex-col sm:flex-row justify-center gap-4 select-none w-full bg-red-50 py-4 border border-red-200'>
           <a href="#coffee">
-            <div className='group  border border-black/10 shadow-xl py-2 px-8 rounded-lg flex md:gap-4 items-center hover:bg-red-700 hover:text-white  transition-all duration-300 ease-in-out '>
+            <div className='group  border border-red-300 bg-white text-red-800 py-2 px-8 rounded-lg flex md:gap-4 items-center hover:bg-red-600 hover:text-white  transition-all duration-300 ease-in-out '>
                 <Coffee className='group-hover:scale-110 w-8 h-8 transition-all duration-300 ease-in-out'/>
-                <p className='text-center max-w-30'> premium coffee</p>
+                <p className='text-center max-w-30 font-semibold'> premium coffee</p>
             </div>
           </a>
           <a href="">
-            <div className='group bg-white/10 border border-black/10 shadow-xl py-2 px-8 rounded-lg flex md:gap-4 items-center hover:bg-red-700 hover:text-white transition-all duration-300 ease-in-out'>
+            <div className='group  border border-red-300 bg-white text-red-800  py-2 px-8 rounded-lg flex md:gap-4 items-center hover:bg-red-600 hover:text-white transition-all duration-300 ease-in-out'>
                 <Cuboid className='group-hover:scale-110 w-8 h-8 transition-all duration-300 ease-in-out'/>
                 <p className='text-center max-w-30'> trading products</p>
             </div>
           </a>
           <a href="#cafe">
-            <div className='group bg-white/10 border border-black/10 shadow-xl py-2 px-8 rounded-lg flex md:gap-4 items-center hover:bg-red-700 hover:text-white transition-all duration-300 ease-in-out'>
+            <div className='group  border border-red-300 bg-white text-red-800  py-2 px-8 rounded-lg flex md:gap-4 items-center hover:bg-red-600 hover:text-white transition-all duration-300 ease-in-out'>
                 <Globe className='group-hover:scale-110 w-8 h-8 transition-all duration-300 ease-in-out'/>
                 <p className='text-center max-w-30'> services</p>
             </div>
@@ -104,7 +104,7 @@ const Products = () => {
           </p>
           {/* coffee cards */}
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-          {coffeeProducts.map((coffee,key)=>(<div key={key} className="flex flex-col rounded-2xl overflow-hidden max-w-[600px] bg-white mb-2  shadow-lg border border-black/30 pb-4">
+          {coffeeProducts.map((coffee,key)=>(<div key={key} className="flex flex-col rounded-2xl overflow-hidden max-w-[600px] bg-white mb-2  shadow-lg border border-red-400 pb-4 hover:scale-102 transition-all duration-400 ease-in-out">
             <div  style={{ backgroundImage: `url(${coffee.image})` }} className={` bg-cover   h-[200px] relative `}>
             <span  className="bg-yellow-400 px-1 py-0.5 rounded-full mt-3 mr-4  absolute top-0 right-0 ">{coffee.grade}</span>
           <p className="flex flex-col gap-1 absolute bottom-0 left-0 pb-2 pl-5  text-white">
@@ -141,7 +141,7 @@ const Products = () => {
           </div>
 
           {/* from bean to cup Exellence */}
-          <div className="flex flex-col items-center gap-4 mt-6 w-full mx-15 bg-white p-8 rounded-2xl ">
+          <div className="flex flex-col items-center gap-4 mt-6 w-full mx-15 bg-red-50 p-8 rounded-2xl border border-red-200 shadow-xl">
             <h1 className="text
             text-3xl font-bold">
           From Bean To Cup Exellence
@@ -151,8 +151,8 @@ const Products = () => {
             </p>
             <div className="flex  md:justify-around md:flex-row flex-col w-full ">
               <div className="flex flex-col items-center gap-4">
-                <div className='bg-red-50  p-2 rounded-full '>
-                                    <MountainIcon className='w-14 h-14 p-3 text-red-600'/>
+                <div className='bg-red-600  p-2 rounded-full '>
+                                    <MountainIcon className='w-14 h-14 p-3 text-white'/>
                                 </div>
                                 <h1 className="text-2xl font-bold text-center"> 
                                   Sourcing
@@ -162,8 +162,8 @@ const Products = () => {
                                 </p>
               </div>
               <div className="flex flex-col items-center gap-4">
-                <div className='bg-red-50  p-2 rounded-full '>
-                                    <Shield className='w-14 h-14 p-3 text-red-600'/>
+                <div className='bg-red-600  p-2 rounded-full '>
+                                    <Shield className='w-14 h-14 p-3 text-white'/>
                                 </div>
                                 <h1 className="text-2xl font-bold text-center"> 
                               Quality control
@@ -173,8 +173,8 @@ const Products = () => {
                                 </p>
               </div>
               <div className="flex flex-col items-center gap-4">
-                <div className='bg-red-50  p-2 rounded-full '>
-                                    <BoxIcon className='w-14 h-14 p-3 text-red-600'/>
+                <div className='bg-red-600  p-2 rounded-full '>
+                                    <BoxIcon className='w-14 h-14 p-3 text-white'/>
                                 </div>
                                 <h1 className="text-2xl font-bold text-center"> 
                                 Processing
@@ -184,8 +184,8 @@ const Products = () => {
                                 </p>
               </div>
               <div className="flex flex-col items-center gap-4">
-                <div className='bg-red-50  p-2 rounded-full '>
-                                    <Globe className='w-14 h-14 p-3 text-red-600'/>
+                <div className='bg-red-600  p-2 rounded-full '>
+                                    <Globe className='w-14 h-14 p-3 text-white'/>
                                 </div>
                                 <h1 className="text-2xl font-bold text-center"> 
                             Export
@@ -199,13 +199,13 @@ const Products = () => {
           </div>
         </section>
 
-<section id ="cafe" className="grid grid-cols-1 md:grid-cols-2 bg-gray-200 gap-4  px-6 py-6">
-  <div className="grid grid-cols-1 gap-4">
+<section id ="cafe" className="grid grid-cols-1 md:grid-cols-2 bg-gray-50 gap-4  px-12 py-6 ">
+  <div className="grid grid-cols-1 gap-4 px-12">
  {/*    Kingdome house coffee card */}
-<div className="flex flex-col justify-end gap-4  pr-20 pl-8 pt-6 bg-gradient-to-r from-red-600 to-red-800 rounded-xl  py-4">
+<div className="flex flex-col justify-end gap-4  p-8  bg-white border border-red-200 rounded-xl  shadow-lg">
 <div className="flex justify-start gap-4 ">
-  <div className="bg-yellow-300  rounded-full ">
-    <Crown className="w-14 h-14 p-3"/>
+  <div className="bg-red-400/20  rounded-full ">
+    <Crown className="w-14 h-14 p-3 text-red-600"/>
   </div>
 
 <div>
@@ -220,79 +220,79 @@ Kingdom house coffee
   
 {/*  the two cards */}
 <div className="flex gap-3">
-  <div className="flex flex-col items-start w-full bg-gray-100/50 rounded-lg p-3">
+  <div className="flex flex-col items-start w-full bg-red-50 border border-red-200 rounded-lg p-3">
 <Calendar/>
 <p>Opening date</p>
 <time dateTime="2025-9-9">2025, septemeber 9 </time>
   </div>
-  <div className="flex flex-col items-start w-full bg-gray-100/50 rounded-lg p-3">
+  <div className="flex flex-col items-start w-full bg-red-50 border border-red-200 rounded-lg p-3">
 <Calendar/>
 <p>Opening date</p>
 <time dateTime="2025-9-9">2025, septemeber 9 </time>
   </div>
 </div>
 </div>
-<div className="grid grid-cols-2 gap-4   p-4">
-  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-lg bg-gradient-to-r from-red-500 to-red-600">
-    <div className=' p-2 rounded-full w-fit bg-black/20 dark:bg-yellow-300/30'>
-            <LucideCoffee className='w-9 h-9 p-1 text-black dark:text-yellow-400  '/>
+<div className="grid grid-cols-2 gap-4">
+  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-2xl bg-white border border-red-200 hover:scale-102 transition-all duration-400 ease-in-out shadow-lg">
+    <div className=' p-2 rounded-full w-fit bg-red-400/20 dark:bg-yellow-300/30'>
+            <LucideCoffee className='w-9 h-9 p-1 text-red-600 dark:text-yellow-400  '/>
      </div>
-     <h1 className="text-2xl font-bold">
+     <h1 className="text-xl font-bold">
 Authentic Ethiopian coffee
      </h1>
      <p>
       traditional coffee ceremonies and premium single-origin brews
      </p>
      </div>
-  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-lg bg-gradient-to-r from-red-600 to-red-800">
-    <div className=' p-2 rounded-full w-fit bg-black/20 dark:bg-yellow-300/30'>
-            <ForkKnife className='w-9 h-9 p-1 text-black dark:text-yellow-400 '/>
+  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-2xl bg-white border border-red-200 hover:scale-102 transition-all duration-400 ease-in-out shadow-lg">
+    <div className=' p-2 rounded-full w-fit bg-red-400/20 dark:bg-yellow-300/30'>
+            <ForkKnife className='w-9 h-9 p-1 text-red-600 dark:text-yellow-400 '/>
      </div>
-     <h1 className="text-2xl font-bold text-center">
+     <h1 className="text-xl font-bold text-center">
 Ethiopian Cusine
      </h1>
      <p>
       Authentic  dishes showcasing  Ethiopia's rich culinary heritage
        </p>
      </div>
-  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-lg bg-gradient-to-r from-red-600 to-red-800">
-    <div className=' p-2 rounded-full w-fit bg-black/20 dark:bg-yellow-300/30'>
-            <Users className='w-9 h-9 p-1 text-black dark:text-yellow-400 '/>
+  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-2xl bg-white border border-red-200 hover:scale-102 transition-all duration-400 ease-in-out shadow-lg">
+    <div className=' p-2 rounded-full w-fit bg-red-400/20 dark:bg-yellow-300/30'>
+            <Users className='w-9 h-9 p-1 text-red-600 dark:text-yellow-400 '/>
      </div>
-     <h1 className="text-2xl font-bold text-center">
+     <h1 className="text-xl font-bold text-center">
 Cultural Experience 
      </h1>
      <p>
   Immersive environment celebrating Ethiopian culture and traditions
      </p>
      </div>
-  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-lg bg-gradient-to-r from-red-600 to-red-800">
-    <div className=' p-2 rounded-full w-fit bg-black/20 dark:bg-yellow-300/30'>
-            <Wifi className='w-9 h-9 p-1 text-black dark:text-yellow-400 '/>
+  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-2xl bg-white border border-red-200 hover:scale-102 transition-all duration-400 ease-in-out shadow-lg">
+    <div className=' p-2 rounded-full w-fit bg-red-400/20 dark:bg-yellow-300/30'>
+            <Wifi className='w-9 h-9 p-1 text-red-600 dark:text-yellow-400 '/>
      </div>
-     <h1 className="text-2xl font-bold text-center">
+     <h1 className="text-xl font-bold text-center">
 Business Hub 
      </h1>
      <p>
 Modern amenities for meeting,work and networking
      </p>
      </div>
-  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-lg bg-gradient-to-r from-red-600 to-red-800">
-    <div className=' p-2 rounded-full w-fit bg-black/20 dark:bg-yellow-300/30'>
-            <Music className='w-9 h-9 p-1 text-black  dark:text-yellow-400 '/>
+  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-2xl bg-white border border-red-200 hover:scale-102 transition-all duration-400 ease-in-out shadow-lg">
+    <div className=' p-2 rounded-full w-fit bg-red-400/20 dark:bg-yellow-300/30'>
+            <Music className='w-9 h-9 p-1 text-red-600 dark:text-yellow-400 '/>
      </div>
-     <h1 className="text-2xl font-bold text-center">
+     <h1 className="text-xl font-bold text-center">
 Entertainment
      </h1>
      <p>
   Movies,shows,news and other form of Entertainment
      </p>
      </div>
-  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-lg bg-gradient-to-r from-red-600 to-red-800">
-    <div className=' p-2 rounded-full w-fit  bg-black/20 dark:bg-yellow-300/30'>
-            <Heart className='w-9 h-9 p-1 text-black dark:text-yellow-400 '/>
+  <div className=" flex items-start  flex-col bg-gray-400/20 p-4 rounded-2xl bg-white border border-red-200 hover:scale-102 transition-all duration-400 ease-in-out shadow-lg">
+    <div className=' p-2 rounded-full w-fit  bg-red-400/20 dark:bg-yellow-300/30'>
+            <Heart className='w-9 h-9 p-1 text-red-600 dark:text-yellow-400 '/>
      </div>
-     <h1 className="text-2xl font-bold text-center">
+     <h1 className="text-xl font-bold text-center">
 Community Space
      </h1>
      <p>
@@ -312,7 +312,7 @@ Bringing together coffee lovers and cultural  enthusiasts
 </section> 
 
 {/* stay updated */}
-<div className="flex flex-col  items-center gap-6 w-full  bg-gradient-to-r from-red-600 to-red-700   pt-8  pb-4">
+<div className="flex flex-col  items-center gap-6 w-full  border-b border-red-200 py-12 mb-4 ">
   <h1 className="text-3xl  font-bold">
 Stay Updated
   </h1>
@@ -320,19 +320,19 @@ Stay Updated
     Be first to  know about our cafe opening, special events and exclusive 
   </p>
   <div  className="flex justify-center gap-4 w-2/3 ">
- <input type="email" className="pl-4 py-4 rounded-lg  bg-white w-2/5"  placeholder="enter your email" name="" id="" />
- <button className=" flex justify-around py-4 px-5 gap-1 bg-black/90 w-1/5 text-white rounded-lg">
+ <input type="email" className="pl-4 py-4 rounded-lg  bg-white w-2/5 border border-red-200"  placeholder="enter your email" name="" id="" />
+ <button className=" flex justify-around py-4 px-5 gap-1 bg-gradient-to-r from-red-600 to-red-800 w-1/5 text-white rounded-lg">
   Notify me <ArrowRight/>
  </button>
   </div>
 
 </div>
 {/* ready to partner to us */}
-<div className="flex flex-col items-center gap-7 my-4 max-w-[600px]">
-  <h1 className="text-2xl font-bold text-center">Ready to Partner with us?</h1>
-  <p className="text-center">Whether you're interested in our premium coffee, trading products, professional services, or excited about our upcoming cafe, we're here to build lasting partnerships that benefit communities and markets worldwide.</p>
+<div className="flex flex-col items-center gap-7 my-4 py-12 max-w-[800px]">
+  <h1 className="text-6xl font-bold text-center">Ready to Partner with us?</h1>
+  <p className="text-center text-lg font-light text-gray-700">Whether you're interested in our premium coffee, trading products, professional services, or excited about our upcoming cafe, we're here to build lasting partnerships that benefit communities and markets worldwide.</p>
 <div className="flex justify-center gap-4">
-  <button className="flex justify-around gap-3 px-4 py-4 bg-white rounded-xl border border-gray-300 cursor-pointer hover:bg-red-600 hover:text-white">
+  <button className="flex justify-around gap-3 px-4 py-4 bg-white rounded-xl border border-red-300 cursor-pointer hover:bg-red-600 hover:text-white hover:scale-103 transition duration:400 shadow-lg">
     <p>Request product Catalogue</p>
     <CuboidIcon/>
   </button>
