c41b9b92d350be60aa1506769a335287715673bf
Kdus17 <kidus2460@gmail.com>
2025-07-28 10:28:03 +0300

    added the email button

--- src/pages/Admin/Add.jsx
+++ src/pages/Admin/Add.jsx
@@ -11,7 +11,7 @@ export default function Add() {
   const [see1, setsee1] = useState(false)
   const [see2, setsee2] = useState(false)
   const [see3, setsee3] = useState(false)
-  const [see4, setsee4] = useState(true)
+  const [see4, setsee4] = useState(false)
   return (
     <div className='flex flex-col items-center'>
     {see4 &&
@@ -35,30 +35,33 @@ export default function Add() {
         </div>
 
         {/**Filters */}
-        <div className='bg-red-50 flex gap-2 py-4 border-red-200 border-b-1 px-24'>
-            <div className={`flex justify-around ${see ? 'bg-red-600 text-white' : 'bg-white'} py-3 px-4 rounded-xl border-1 border-red-200 gap-4 items-center select-none`}  onClick={()=>{
-                if(see === false){
-                    setsee(true)
-                    setsee1(false)
-                }
-            }}>
-                <Coffee/>
-                <span className=' font-semibold'>Coffee Products</span>
-                {/* <div className='bg-yellow-200/60 text-red-800 text-sm font-semibold rounded-full  flex justify-center items-center w-5 h-5'>
-                    <span className={`${see ? 'text-white':''}`}>2</span>
-                </div> */}
+        <div className='bg-red-50 flex justify-between py-4 border-red-200 border-b-1 px-24'>
+            <div className='flex gap-2'>
+                <div className={`flex justify-around ${see ? 'bg-red-600 text-white' : 'bg-white'} py-3 px-4 rounded-xl border-1 border-red-200 gap-4 items-center select-none`}  onClick={()=>{
+                    if(see === false){
+                        setsee(true)
+                        setsee1(false)
+                    }
+                }}>
+                    <Coffee/>
+                    <span className=' font-semibold'>Coffee Products</span>
+                </div>
+                <div className={`flex justify-around ${see1 ? 'bg-red-600 text-white' : 'bg-white'} py-3 px-4 rounded-xl border-1 border-red-200 gap-4 items-center select-none`} onClick={()=>{
+                    if(see1 === false){
+                        setsee1(true)
+                        setsee(false)
+                    }
+                }}>
+                    <Box/>
+                    <span className=' font-semibold'>Trading Products</span>
+                </div>
             </div>
-            <div className={`flex justify-around ${see1 ? 'bg-red-600 text-white' : 'bg-white'} py-3 px-4 rounded-xl border-1 border-red-200 gap-4 items-center select-none`} onClick={()=>{
-                if(see1 === false){
-                    setsee1(true)
-                    setsee(false)
-                }
-            }}>
-                <Box/>
-                <span className=' font-semibold'>Trading Products</span>
-                {/* <div className='bg-red-200/30 text-red-800 text-sm font-semibold font-bold rounded-full  flex justify-center items-center w-5 h-5'>
-                    <span className={`${see1 ? 'text-white':''}`}>2</span>
-                </div> */}
+            <div className={`flex justify-around self-end bg-white  py-3 px-4 rounded-xl border-1 border-red-200 gap-4 items-center select-none`} onClick={()=>{
+                    if(see4 === false){
+                        setsee4(true)
+                    }
+                }}>
+                Compose Email
             </div>
         </div>
 
