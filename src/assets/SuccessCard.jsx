aa3f7da565b765266f66fb16b8b54fb07211abc3
Kdus17 <kidus2460@gmail.com>
2025-08-11 09:42:07 +0300

    success card fix

--- src/assets/SuccessCard.jsx
+++ src/assets/SuccessCard.jsx
@@ -2,7 +2,7 @@ import { CheckCircle2 } from 'lucide-react'
 
 export default function SuccessCard({Success}) {
   return (
-      <div className="absolute bottom-0 flex items-center bg-green-100 text-green-800 border-2 border-green-500 px-1 py-2 rounded-md w-full select-none">
+      <div className="flex items-center bg-green-100 text-green-800 border-2 border-green-500 px-1 py-2 rounded-md w-full select-none">
         <CheckCircle2 className=""/>
         <span className="ml-2">{Success}</span>
       </div>
